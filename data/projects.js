00471cd4aafcfb9357d87ee958469c580aa0143c
idKrazu <idkrazu@gmail.com>
2021-11-18 21:07:49 +0100

    ✨ Maping projects

--- data/projects.js
+++ data/projects.js
@@ -2,7 +2,42 @@ const projects = [
   {
     id: 1,
     title: "Kebab Pod Kauflandem",
-    body: "",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, expedita sequi incidunt maxime, laborum libero non consequatur temporibus vero ut, aliquid earum. Recusandae, cumque rerum ipsa reprehenderit labore nisi repellendus.",
+    date: "October 2021",
+    stack: ["React", "HTML", "CSS", "JavaScript"],
+    links: {
+      github: "https://github.com/idKrazu/kebab-pod-kauflandem",
+      live: "https://kebabpodkauflandem.netlify.app/",
+    },
+  },
+  {
+    id: 2,
+    title: "Kebab Pod Kauflandem",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores laborum officia quos sed eum numquam nemo aperiam cum distinctio maxime eius deserunt recusandae voluptatem cumque consequatur, fugit, tempore, animi obcaecati!",
+    date: "October 2021",
+    stack: ["React", "HTML", "CSS", "JavaScript"],
+    links: {
+      github: "https://github.com/idKrazu/kebab-pod-kauflandem",
+      live: "https://kebabpodkauflandem.netlify.app/",
+    },
+  },
+  {
+    id: 3,
+    title: "Kebab Pod Kauflandem",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores laborum officia quos sed eum numquam nemo aperiam cum distinctio maxime eius deserunt recusandae voluptatem cumque consequatur, fugit, tempore, animi obcaecati!",
+    date: "October 2021",
+    stack: ["React", "HTML", "CSS", "JavaScript"],
+    links: {
+      github: "https://github.com/idKrazu/kebab-pod-kauflandem",
+      live: "https://kebabpodkauflandem.netlify.app/",
+    },
+  },
+  {
+    id: 4,
+    title: "Kebab Pod Kauflandem",
+    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores laborum officia quos sed eum numquam nemo aperiam cum distinctio maxime eius deserunt recusandae voluptatem cumque consequatur, fugit, tempore, animi obcaecati!",
+    date: "October 2021",
+    stack: ["React", "HTML", "CSS", "JavaScript"],
     links: {
       github: "https://github.com/idKrazu/kebab-pod-kauflandem",
       live: "https://kebabpodkauflandem.netlify.app/",
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,45 @@
+import projects from "../../data/projects";
+import { ExternalLink, GitHub } from "react-feather";
+
 const Projects = () => {
+  const techStack = (arr) => {
+    arr.map((tech) => {
+      <div key={tech}>{tech}</div>;
+    });
+  };
+
+  const projectsBox = projects.map((project) => (
+    <div className="project" key={project.id}>
+      <h3>{project.title}</h3>
+      <p>{project.date}</p>
+      <div className="project-links">
+        <a
+          href={project.links.github}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          <GitHub strokeWidth="1.5px" />
+        </a>
+        <a href={project.links.live} target="_blank" rel="noopener noreferrer">
+          {" "}
+          <ExternalLink strokeWidth="1.5px" />
+        </a>
+      </div>
+      <p>{project.body}</p>
+      <div>
+        {project.stack.map((tech) => (
+          <div className="technology" key={tech}>
+            {tech}
+          </div>
+        ))}
+      </div>
+    </div>
+  ));
   return (
-    <div>
+    <div className="projects">
       <h2>My Projects.</h2>
-      <p>Projects stuff</p>
+      {projectsBox}
     </div>
   );
 };
