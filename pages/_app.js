efb89fb6c04f69a60327e7228685e03a2349c932
idKrazu <idkrazu@gmail.com>
2021-11-23 18:10:48 +0100

    ✨ Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,24 @@ import transaltions from "../data/translations";
 import { AnimateSharedLayout } from "framer-motion";
 import { useEffect, useState } from "react";
 import cookieCutter from "cookie-cutter";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  // Google Analytics
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  // Lang switcher
   const [langPolish, setLangPolish] = useState(true);
   const [translationsObj, setTranslations] = useState(transaltions.pl);
   const switchLang = () => {
@@ -44,15 +60,36 @@ function MyApp({ Component, pageProps }) {
 
   pageProps = { translationsObj, ...pageProps };
   return (
-    <Layout
-      langPolish={langPolish}
-      switchLang={switchLang}
-      translationsObj={translationsObj}
-    >
-      <AnimateSharedLayout>
-        <Component {...pageProps} />
-      </AnimateSharedLayout>
-    </Layout>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Layout
+        langPolish={langPolish}
+        switchLang={switchLang}
+        translationsObj={translationsObj}
+      >
+        <AnimateSharedLayout>
+          <Component {...pageProps} />
+        </AnimateSharedLayout>
+      </Layout>
+    </>
   );
 }
 
